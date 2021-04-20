17fa89a931a65cd0a7dc0de91715ed73054c3d7d
unknown <a175328@siswa.ukm.edu.my>
2021-04-20 11:20:08 +0800

    Update website

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Manage {{$user->name}}</div>
+                <div class="card-header">Manage {{$user->name}} Role</div>
 
                 <div class="card-body">
                     <form action="{{route('admin.users.update', ['user'=> $user->id]) }}" method="POST">
@@ -13,14 +13,20 @@
                         {{method_field('PUT') }}
                         @foreach ($roles as $role)
                             <div class="form-check">
+                                <label for="name" class="col-md-5 col-form-label text-md-right">Roles</label>
                                 <input type="checkbox" name="roles[]" value="{{$role->id}}"
                                 {{$user->hasAnyRole($role->name)?'checked':'' }}>
                                 <label>{{$role->name}}</label>
                             </div>
                         @endforeach
-                        <button type="submit" class="btn btn-primary">
-                            Update
-                        </button>
+                            <br>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Update Role') }}
+                                    </button>
+                                </div>
+                            </div>
                     </form>
                 </div>
             </div>
