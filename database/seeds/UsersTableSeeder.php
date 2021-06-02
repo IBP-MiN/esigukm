329bc25bd9d0aa2d4460222ad0f83ab9982a5539
unknown <a175328@siswa.ukm.edu.my>
2021-06-02 08:22:26 +0800

    Update Website

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,7 +17,7 @@ class UsersTableSeeder extends Seeder
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         User::truncate();
         DB::table('role_user')->truncate();
-        
+       
 
         $adminRole = Role::where('name', 'admin')->first();
         $lecturerRole = Role::where('name', 'lecturer')->first();
@@ -78,7 +78,7 @@ class UsersTableSeeder extends Seeder
         $lecturer->roles()->attach($lecturerRole);
         $ajk->roles()->attach($ajkRole);
         $user->roles()->attach($userRole);
-
+        
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
 
     }
